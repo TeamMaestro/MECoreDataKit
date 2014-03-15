d615dd845482a7c41d7e32e75e92e4addde7112f
William Towe <willbur1984@gmail.com>
2014-03-15 15:32:05 -0400

    Initial Commit

---
+++ MECoreDataKit/MECoreDataKit.h
@@ -0,0 +1,13 @@
+//
+//  MECoreDataKit.h
+//  MECoreDataKit
+//
+//  Created by William Towe on 3/15/14.
+//  Copyright (c) 2014 Maestro, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MECoreDataKit : NSObject
+
+@end
